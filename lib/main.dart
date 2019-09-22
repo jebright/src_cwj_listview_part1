f114164fe86d17dacc6bcb8098b65fdca5d9f1b3
joe <ebrightster@gmail.com>
2019-09-22 06:09:15 -0700

    Ready for Blog Post

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ListViewP1',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Static ListView'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +26,175 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final String newHopeImage =
+      'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
+  final String newHopeText =
+      "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String empireStrikesBackImage =
+      'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
+  final String empireStrikesBackText =
+      "It is a dark time for the\r\nRebellion. Although the Death\r\nStar has been destroyed,\r\nImperial troops have driven the\r\nRebel forces from their hidden\r\nbase and pursued them across\r\nthe galaxy.\r\n\r\nEvading the dreaded Imperial\r\nStarfleet, a group of freedom\r\nfighters led by Luke Skywalker\r\nhas established a new secret\r\nbase on the remote ice world\r\nof Hoth.\r\n\r\nThe evil lord Darth Vader,\r\nobsessed with finding young\r\nSkywalker, has dispatched\r\nthousands of remote probes into\r\nthe far reaches of space....";
+
+  final String returnOfTheJediImage =
+      'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg';
+  final String returnOfTheJediText =
+      "Luke Skywalker has returned to\r\nhis home planet of Tatooine in\r\nan attempt to rescue his\r\nfriend Han Solo from the\r\nclutches of the vile gangster\r\nJabba the Hutt.\r\n\r\nLittle does Luke know that the\r\nGALACTIC EMPIRE has secretly\r\nbegun construction on a new\r\narmored space station even\r\nmore powerful than the first\r\ndreaded Death Star.\r\n\r\nWhen completed, this ultimate\r\nweapon will spell certain doom\r\nfor the small band of rebels\r\nstruggling to restore freedom\r\nto the galaxy...";
+
+  final String thePhantomMenaceImage =
+      'https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg';
+  final String thePhantomMenaceText =
+      "Turmoil has engulfed the\r\nGalactic Republic. The taxation\r\nof trade routes to outlying star\r\nsystems is in dispute.\r\n\r\nHoping to resolve the matter\r\nwith a blockade of deadly\r\nbattleships, the greedy Trade\r\nFederation has stopped all\r\nshipping to the small planet\r\nof Naboo.\r\n\r\nWhile the Congress of the\r\nRepublic endlessly debates\r\nthis alarming chain of events,\r\nthe Supreme Chancellor has\r\nsecretly dispatched two Jedi\r\nKnights, the guardians of\r\npeace and justice in the\r\ngalaxy, to settle the conflict....";
+
+  final String attackOfTheClonesImage =
+      'https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg';
+  final String attackOfTheClonesText =
+      "There is unrest in the Galactic\r\nSenate. Several thousand solar\r\nsystems have declared their\r\nintentions to leave the Republic.\r\n\r\nThis separatist movement,\r\nunder the leadership of the\r\nmysterious Count Dooku, has\r\nmade it difficult for the limited\r\nnumber of Jedi Knights to maintain \r\npeace and order in the galaxy.\r\n\r\nSenator Amidala, the former\r\nQueen of Naboo, is returning\r\nto the Galactic Senate to vote\r\non the critical issue of creating\r\nan ARMY OF THE REPUBLIC\r\nto assist the overwhelmed\r\nJedi....";
+
+  final String revengeOfTheSithImage =
+      'https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg';
+  final String revengeOfTheSithText =
+      "War! The Republic is crumbling\r\nunder attacks by the ruthless\r\nSith Lord, Count Dooku.\r\nThere are heroes on both sides.\r\nEvil is everywhere.\r\n\r\nIn a stunning move, the\r\nfiendish droid leader, General\r\nGrievous, has swept into the\r\nRepublic capital and kidnapped\r\nChancellor Palpatine, leader of\r\nthe Galactic Senate.\r\n\r\nAs the Separatist Droid Army\r\nattempts to flee the besieged\r\ncapital with their valuable\r\nhostage, two Jedi Knights lead a\r\ndesperate mission to rescue the\r\ncaptive Chancellor....";
+
+  final String theForceAwakensImage =
+      'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg';
+  final String theForceAwakensText =
+      "Luke Skywalker has vanished.\r\nIn his absence, the sinister\r\nFIRST ORDER has risen from\r\nthe ashes of the Empire\r\nand will not rest until\r\nSkywalker, the last Jedi,\r\nhas been destroyed.\r\n \r\nWith the support of the\r\nREPUBLIC, General Leia Organa\r\nleads a brave RESISTANCE.\r\nShe is desperate to find her\r\nbrother Luke and gain his\r\nhelp in restoring peace and\r\njustice to the galaxy.\r\n \r\nLeia has sent her most daring\r\npilot on a secret mission\r\nto Jakku, where an old ally\r\nhas discovered a clue to\r\nLuke's whereabouts....";
+
+  final String theLastJediImage =
+      'https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg';
+  final String theLastJediText =
+      "The FIRST ORDER reigns.\r\nHaving decimated the peaceful Republic,\r\nSupreme Leader Snoke now\r\ndeploys his merciless legions\r\nto seize military control of\r\nthe galaxy.\r\n\r\nOnly General Leia Organa’s band\r\nof RESISTANCE fighters stand\r\nagainst the rising tyranny, certain\r\nthat Jedi Master Luke Skywalker\r\nwill return and restore a spark of\r\nhope to the fight.\r\n\r\nBut the Resistance has been exposed.\r\nAs the First Order speeds\r\ntoward the rebel base,\r\nthe brave heroes mount a desperate escape....";
+
+  final String theRiseOfSkywalkerImage =
+      'https://m.media-amazon.com/images/M/MV5BNDA5YWU1MDItOWZlOS00YTljLThhYTYtMzM4MGI2NjhlMDYwXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_SX300.jpg';
+  final String theRiseOfSkywalkerText = "Coming soon...";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: body(context));
+  }
+
+  ExpansionTile starWarsTile(String imgUrl, String title, String description) {
+    return ExpansionTile(
+        leading: Image.network(imgUrl),
+        title: Text(title),
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0), child: Text(description))
+        ]);
+  }
+/*
+  body(BuildContext context) {
+    return ListView(
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: [
+          ExpansionTile(
+              leading: Image.network(newHopeImage),
+              title: Text('A New Hope'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(newHopeText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(empireStrikesBackImage),
+              title: Text('The Empire Strikes Back'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(empireStrikesBackText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(returnOfTheJediImage),
+              title: Text('Return of the Jedi'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(returnOfTheJediText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(thePhantomMenaceImage),
+              title: Text('The Phantom Menace'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(thePhantomMenaceText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(attackOfTheClonesImage),
+              title: Text('Attack of the Clones'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(attackOfTheClonesText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(revengeOfTheSithImage),
+              title: Text('Revenge of the Sith'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(revengeOfTheSithText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(theForceAwakensImage),
+              title: Text('The Force Awakens'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(theForceAwakensText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(theLastJediImage),
+              title: Text('The Last Jedi'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(theLastJediText))
+              ]),
+          ExpansionTile(
+              leading: Image.network(theRiseOfSkywalkerImage),
+              title: Text('The Rise of Skywalker'),
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(theRiseOfSkywalkerText))
+              ]),
+        ],
+      ).toList(),
     );
   }
+  */
+
+  body(BuildContext context) {
+    return ListView(
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: [
+          starWarsTile(newHopeImage, 'A New Hope', newHopeText),
+          starWarsTile(empireStrikesBackImage, 'The Empire Strikes Back',
+              empireStrikesBackText),
+          starWarsTile(
+              returnOfTheJediImage, 'Return of the Jedi', returnOfTheJediText),
+          starWarsTile(thePhantomMenaceImage, 'The Phantom Menace',
+              thePhantomMenaceText),
+          starWarsTile(attackOfTheClonesImage, 'Attack of the Clones',
+              attackOfTheClonesText),
+          starWarsTile(revengeOfTheSithImage, 'Revenge of the Sith',
+              revengeOfTheSithText),
+          starWarsTile(
+              theForceAwakensImage, 'The Force Awakens', theForceAwakensText),
+          starWarsTile(theLastJediImage, 'The Last Jedi', theLastJediText),
+          starWarsTile(theRiseOfSkywalkerImage, 'The Rise of Skywalker',
+              theRiseOfSkywalkerText),
+        ],
+      ).toList(),
+    );
+  }
+
 }
